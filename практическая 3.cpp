1bfb5b99853831876fbc00466d7bed6fef93abf2
qwertyzxc123-pro <megadrill708@gmail.com>
2025-10-17 10:20:19 +0300

    Update практическая 3.cpp

--- практическая 3.cpp
+++ практическая 3.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
@@ -18,34 +18,34 @@ int main() {
 
  else {
      if (number >= 1 && number <= 10) {
-         cout << "Диапазон: 1 - 10" << endl;
+         cout << "Цифра в диапазоне: 1 - 10" << endl;
      }
      else if (number >= 11 && number <= 20) {
-         cout << "Диапазон: 11 - 20" << endl;
+         cout << "Цифра в диапазоне: 11 - 20" << endl;
      }
      else if (number >= 21 && number <= 30) {
-         cout << "Диапазон: 21 - 30" << endl;
+         cout << "Цифра в диапазоне: 21 - 30" << endl;
      }
      else if (number >= 31 && number <= 40) {
-         cout << "Диапазон: 31 - 40" << endl;
+         cout << "Цифра в диапазоне: 31 - 40" << endl;
      }
      else if (number >= 41 && number <= 50) {
-         cout << "Диапазон: 41 - 50" << endl;
+         cout << "Цифра в диапазоне: 41 - 50" << endl;
      }
      else if (number >= 51 && number <= 60) {
-         cout << "Диапазон: 51 - 60" << endl;
+         cout << "Цифра в диапазоне: 51 - 60" << endl;
      }
      else if (number >= 61 && number <= 70) {
-         cout << "Диапазон: 61 - 70" << endl;
+         cout << "Цифра в диапазоне: 61 - 70" << endl;
      }
      else if (number >= 71 && number <= 80) {
-         cout << "Диапазон: 71 - 80" << endl;
+         cout << "Цифра в диапазоне: 71 - 80" << endl;
      }
      else if (number >= 81 && number <= 90) {
-         cout << "Диапазон: 81 - 90" << endl;
+         cout << "Цифра в диапазоне: 81 - 90" << endl;
      }
      else if (number >= 91 && number <= 100) {
-         cout << "Диапазон: 91 - 100" << endl;
+         cout << "Цифра в диапазоне: 91 - 100" << endl;
      }
     }
 
