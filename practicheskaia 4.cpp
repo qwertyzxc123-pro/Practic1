3f9f80d7b5c42f9c115c1ffd3f83888e34008a19
qwertyzxc123-pro <megadrill708@gmail.com>
2025-10-23 16:24:22 +0300

    Add files via upload

---
+++ practicheskaia 4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int number;
+    int multiplier = 1;
+
+    cout << "Введите число: "; cin >> number;
+    while (multiplier <= 9) {
+        cout << "решите пример:" << number << " x " << multiplier << endl;
+        int answer;
+        cout << "Ответ:"; cin >> answer;
+
+        int correct = number * multiplier;
+        if (answer == correct)
+        {
+            cout << "Пример решен правильно!" << endl;
+            multiplier++;
+        }
+        else {
+            cout << "Ошибка, пример решен неверено!" << endl;
+            break;
+        }
+    }
+    if (multiplier > 9)
+    {
+        cout << "Все примеры решены верно!" << endl;
+    }
+
+
+    return 0;
+}
