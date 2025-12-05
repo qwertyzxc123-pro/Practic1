ea5bd4964c0c967c5a1ebdce255d1a6cadb771fd
qwertyzxc123-pro <megadrill708@gmail.com>
2025-12-05 11:25:59 +0300

    Add files via upload

---
+++ wh1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int number; 
+    int sum = 0;
+
+    cout << "[ + ] Введите число: ";
+    cin >> number; 
+
+    
+    while (number != 0) {
+        sum += number; 
+        cout << "[ + ] Введите число: ";
+        cin >> number; 
+    }
+
+    cout << "[ + ] Сумма: " << sum << endl;
+
+    return 0;
+}
