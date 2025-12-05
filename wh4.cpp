5ec0b369b4788c14cb92c0e39f932de7a8157f44
qwertyzxc123-pro <megadrill708@gmail.com>
2025-12-05 12:44:16 +0300

    Add files via upload

---
+++ wh4.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+	int number;
+	int count = 0;
+
+	cout << "[+] Введите число: ";
+	cin >> number;
+
+	if (number == 0) {
+		count = 1;
+	}
+	else {
+		int temp = number;
+
+		while (temp > 0) {
+			count++;
+
+			temp = temp / 10;
+
+
+		}
+	}
+
+	cout << "[+] Количество цифр: "
+<< count << endl;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
