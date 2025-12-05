3d8113ca5d6fff9e9faff5aa304b3357c3970b0a
qwertyzxc123-pro <megadrill708@gmail.com>
2025-12-05 11:35:43 +0300

    Add files via upload

---
+++ wh2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int number;
+
+    cout << "[ + ] Введите число: ";
+    cin >> number;
+
+    cout << "Отсчет:\n";
+
+    while (number > 0) {
+        cout << "Отсчет:" << number << endl;
+        number--; 
+    }
+
+    cout << "[ + ] Старт!\n";
+
+    return 0;
+}
