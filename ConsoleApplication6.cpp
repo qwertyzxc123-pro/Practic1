1c8e2b321d1dbd7099cbff1b0dd6673c6ca3be40
qwertyzxc123-pro <megadrill708@gmail.com>
2025-12-19 12:20:51 +0300

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int num;
+
+    cout << "[ + ] Введите число: ";
+    cin >> num;
+
+    for (int i = 1; i <= 10; i++) {
+        cout << "[+]" << num << " × " << i << " = " << num * i << endl;
+    }
+
+    return 0;
+}
